6b49f41ba2b83ffd90090a7c1e6870bf1b702280
arafat593 <mdalarafat593@gmail.com>
2025-06-12 15:53:00 +0600

    add file

---
+++ lib/Zidney_web.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:khal971_zidney_course_webapp/views/dashbord/dashbord.dart';
+
+class ZidneyWeb extends StatelessWidget {
+  const ZidneyWeb({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Dashbord(),
+    );
+  }
+}
---
+++ lib/core/utils/asset_path.dart
@@ -0,0 +1,5 @@
+class AssetPath {
+  static const String _basePath = 'assets/images';
+  static const String appLogo = '$_basePath/logo.png';
+
+}
